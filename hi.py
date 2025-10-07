c9d8e39b7866205ec25af27afb635a33ab31a87e
marinqjx-j <marinaplanert@gmail.com>
2025-10-07 09:08:09 +0200

    Update hi.py

--- hi.py
+++ hi.py
@@ -11,6 +11,7 @@ screen = pygame.display.set_mode((breite, höhe))
 pygame.display.set_caption("Game")
 
 player = pygame.image.load("play.png").convert_alpha()
+player_rect = player.get_rect(topleft=(0, 0))
 
 clock = pygame.time.Clock()
 
@@ -31,13 +32,14 @@ move_down = False
 
 run = True
 
+
 while run == True:
     screen.fill(HG)
     pygame.draw.rect(screen, (0, 255, 0), box, width=2)
 
     keys = pygame.key.get_pressed()
 
-    if player.colliderect(box):
+    if player_rect.colliderect(box):
         if keys[pygame.K_SPACE] and space_released:
             space_released = False
             index = (index + 1) if (index + 1) != len(text_renders) else 0
@@ -49,8 +51,8 @@ while run == True:
     if index != -1:
         screen.blit(text_renders[index], (0, 0))
 
-
-    screen.blit(player, (0,0))
+    screen.blit(player, player_rect)
+    ...
     for event in pygame.event.get():
         if(event.type == pygame.KEYDOWN):
             if(event.key == pygame.K_RIGHT):
