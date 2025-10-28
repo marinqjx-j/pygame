271cbbb64c04330b3866d9cd81cbb9fd5f42d935
marinqjx-j <marinaplanert@gmail.com>
2025-10-28 09:34:43 +0100

    Refactor game dimensions and enhance visuals
    Updated game dimensions and added new images for background and dialogue panel.

--- game.py
+++ game.py
@@ -4,10 +4,10 @@ import time
 
 pygame.init()
 
-breite = 600
-höhe = 600
+width = 400
+height = 400
 
-screen = pygame.display.set_mode((breite, höhe))
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Game")
 
 player = pygame.image.load("play.png").convert_alpha()
@@ -19,11 +19,17 @@ box = pygame.Rect(300, 200, 100, 100)
 
 font = pygame.font.SysFont('Times New Roman', 20)
 dialogue = ["Where's my friend?", "Hi.", "What are you?!"]
-text_renders = [font.render(text, True, (0, 0, 255)) for text in dialogue]
+text_renders = [font.render(text, True, (172,147, 98)) for text in dialogue]
 index = -1
 space_released = True
 
-HG = (184, 15, 10)
+HG = pygame.image.load("raum_von_player.png").convert_alpha()
+HG_rect = HG.get_rect(topleft=(0, 0))
+
+lala = pygame.image.load("lala.png").convert_alpha()
+lala_rect = lala.get_rect(topleft=(0,0))
+
+#
 
 move_left = False
 move_right = False
@@ -32,12 +38,8 @@ move_down = False
 
 run = True
 
-
 while run:
-    screen.fill(HG)
-    pygame.draw.rect(screen, (0, 255, 0), box, width=2)
-
-    # Handle movement
+    # movement
     speed = 5
     keys = pygame.key.get_pressed()
     if keys[pygame.K_RIGHT]:
@@ -49,7 +51,12 @@ while run:
     if keys[pygame.K_DOWN]:
         player_rect.y += speed
 
-    # Dialogue logic
+    screen.blit(HG, HG_rect)
+    screen.blit(lala, lala_rect)
+    screen.blit(player, player_rect)
+    pygame.draw.rect(screen, (172, 147, 98), box, width=2)
+
+    # dialogue logic
     if player_rect.colliderect(box):
         if keys[pygame.K_SPACE] and space_released:
             space_released = False
@@ -59,10 +66,31 @@ while run:
     else:
         index = -1
 
+    # dialogue background(panel)
     if index != -1:
-        screen.blit(text_renders[index], (0, 0))
+        text_surface = text_renders[index]
+        padding = 10
 
-    screen.blit(player, player_rect)
+        panel_w = text_surface.get_width() + padding * 2
+        panel_h = text_surface.get_height() + padding * 2
+
+    panel_img = pygame.image.load("panel.png").convert_alpha()
+    panel = pygame.transform.smoothscale(panel_img, (panel_w, panel_h))
+
+    panel.blit(text_surface, (padding, padding))
+
+    panel_x = (width - panel_w) // 2
+    panel_y = height - panel_h - 20
+    screen.blit(panel, (panel_x, panel_y))
+
+    #if knife_rect.colliderect(lala):
+         #lala verliet ein leben
+
+    #if lala_rect.colliderect(player):
+         #player verliert ein leben
+
+    #if leben_player < 1:
+         #game over
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -70,3 +98,6 @@ while run:
 
     pygame.display.update()
     clock.tick(60)
+
+pygame.quit()
+sys.exit()
